e555d5f7ca7343e4d373968c9cdc49c860718381
parthkathpal10 <kathpalparth10@gmail.com>
2026-08-17 18:08:20 +0530

    Ninth code

---
+++ list.py
@@ -0,0 +1,9 @@
+movies = []
+movie1 = str(input("Enter your first favorite movie: "))
+movie2 = str(input("Enter your second favorite movie: "))
+movie3 = str(input("Enter your third favorite movie: "))
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
