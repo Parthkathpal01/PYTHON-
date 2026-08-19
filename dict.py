463f8f9397f4078df0fa2f91bc4dff0f61a9bb7a
parthkathpal10 <kathpalparth10@gmail.com>
2026-08-19 17:52:36 +0530

    Fifteenth code

---
+++ dict.py
@@ -0,0 +1,13 @@
+dictionary = {}
+sub1 = float(input("Enter the marks of phy: "))
+dictionary.update({"phy" : sub1 })
+
+
+sub2 = float(input("Enter the marks of chem: "))
+dictionary.update({"chem" : sub2 })
+
+
+sub3 = float(input("Enter the marks of math: "))
+dictionary.update({"math" : sub3 })
+
+print(dictionary)
