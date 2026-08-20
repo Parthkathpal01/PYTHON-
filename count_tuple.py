1a68fda2d554827564f44109540775032b60a4d7
parthkathpal10 <kathpalparth10@gmail.com>
2026-08-20 16:45:15 +0530

    For loop codes

--- count_tuple.py
+++ count_tuple.py
@@ -1,7 +1,3 @@
 tuple1 = ('C', 'D', 'A', 'A', 'B', 'A')
 print(tuple1.count('A'))
 
-list1 = []
-list1.append(tuple1)
-list.sort(list1)
-print(list1)
---
+++ for_loop.py
@@ -0,0 +1,35 @@
+# print the elements of the following list
+# lists = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for element in lists:
+#     print(element)
+
+
+# search for a number x in this tuple
+# tuples = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# index = 0
+# x = int(input("Enter the element to find: "))
+# for element in tuples:
+#     if(element == x):
+#         print(x ,"at index", index)
+#     index += 1
+    
+
+# for i in range(100):
+#     print(i)
+    
+# for i in range(100, 0, -1):
+#     print (i)
+    
+
+# n= int(input("Enter the number for multiplication table: "))
+# for i in range(1, 11):
+#     print(n * i)
+
+n = int(input("Enter the number: "))
+sum = 0
+i = 1
+while(i <= n):
+    sum = sum + i
+    i += 1
+print(sum)
