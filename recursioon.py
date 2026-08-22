7eebd485a905b05040af4d800567547f0cf83dbb
parthkathpal10 <kathpalparth10@gmail.com>
2026-08-22 14:49:40 +0530

    Recursion codes

---
+++ recursioon.py
@@ -0,0 +1,33 @@
+# def show(n):
+#     if(n == 0):
+#         return
+#     print(n)
+#     show(n-1)
+    
+# show(5)
+
+# def fact(n):
+#     if(n == 0 or n == 1):
+#         return 1
+#     else:
+#         return n * fact(n-1)
+    
+# print(fact(5))
+
+# def calc_sum(n):
+#     if(n == 0):
+#         return 0 
+#     return calc_sum(n - 1) + n
+    
+# sum = calc_sum(5)
+# print(sum)
+
+def print_list(list, index = 0):
+    if(index == len(list)):
+        return
+    print(list[index])
+    print_list(list, index +1)
+    
+fruits = ["Apple", "Banana", "Mango"]
+print_list(fruits)
+    
