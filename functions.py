bc5a0c5d037550e26a80c49de6599ef3e05dac3a
parthkathpal10 <kathpalparth10@gmail.com>
2026-08-22 13:50:19 +0530

    Function codes

---
+++ functions.py
@@ -0,0 +1,46 @@
+# average of 3 numbers
+# def average(a, b, c):
+#     return (a+b+c)/3
+
+# avg = average(3, 5, 7)
+# print(avg)
+
+
+# cities = ["Delhi", "Gurgaon", "Noida", "Pune", "Mumbai", "Chennai"]
+# heroes = ["Thor", "Ironman", "Caiptan America", "Shaktiman"]
+
+# def print_length(list):
+#     print(len(list))
+    
+# print_length(cities)
+# print_length(heroes)
+
+# heroes = ["Thor", "Ironman", "Caiptan America", "Shaktiman"]
+# def print_list(lists):
+#     for i in lists:
+#         print(i, end = " ")
+        
+# print_list(heroes)
+        
+# def fact(n):
+#     facts = 1
+#     for i in range(1, n+1):
+#         facts *= i
+#     print(facts)
+        
+# fact(5)
+
+# def convert_usd_to_inr(usd):
+#     inr = usd * 95.70
+#     print(usd, "USD =", inr, "Rupees")
+    
+# convert_usd_to_inr(25)
+
+
+# def even_or_odd(n):
+#     if n %2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# even_or_odd(10)
